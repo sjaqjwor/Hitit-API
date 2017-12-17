067e1a964ab94ebffabd8f7e699866921cbfb8d4
sjaqjwor <lsklsk45@naver.com>
2017-12-17 16:02:18 +0900

    modify Message.java complete

--- src/main/java/me/hitit/api/domains/Message.java
+++ src/main/java/me/hitit/api/domains/Message.java
@@ -1,7 +1,6 @@
 package me.hitit.api.domains;
 
 import java.sql.Timestamp;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +8,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,12 +24,13 @@ public class Message {
 
 	@ManyToOne
 	@JoinColumn(name = "cidx")
+	@JsonIgnore
 	private User user;
 
 	@Column(name="type")
 	private Type type;
 
-	@Column(name="contents")
+	@Column(name="contents",columnDefinition = "TEXT")
 	private String contents;
 
 	@Column(name = "ts", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
@@ -42,58 +48,5 @@ public class Message {
 		public short getCode() {
 			return code;
 		}
-	}
-
-	public Message() {
-		super();
-	}
-
-	public Message(Long idx, User user, Type type, String contents, Timestamp ts) {
-		super();
-		this.idx = idx;
-		this.user = user;
-		this.type = type;
-		this.contents = contents;
-		this.ts = ts;
-	}
-
-	public Long getIdx() {
-		return idx;
-	}
-
-	public void setIdx(Long idx) {
-		this.idx = idx;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public Type getType() {
-		return type;
-	}
-
-	public void setType(Type type) {
-		this.type = type;
-	}
-
-	public String getContents() {
-		return contents;
-	}
-
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-
-	public Timestamp getTs() {
-		return ts;
-	}
-
-	public void setTs(Timestamp ts) {
-		this.ts = ts;
 	}
 }

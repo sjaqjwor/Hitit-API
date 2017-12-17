c6108e387712a67f3f7f67a086923ab0a6ba1319
CHEOLJIN <kcj091857@gmail.com>
2017-12-17 16:25:10 +0900

    Modify domain TimelineComment.java

--- src/main/java/me/hitit/api/domains/TimelineComment.java
+++ src/main/java/me/hitit/api/domains/TimelineComment.java
@@ -3,19 +3,24 @@ package me.hitit.api.domains;
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.Embeddable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Embeddable
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TimelineComment {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "idx")
@@ -23,26 +28,17 @@ public class TimelineComment {
 
 	@ManyToOne
 	@JoinColumn(name = "tidx")
+	@JsonIgnore
 	private Timeline timeline;
 
 	@ManyToOne
 	@JoinColumn(name = "uidx")
+	@JsonIgnore
 	private User user;
 
-	@Column(name = "contents")
+	@Column(name = "contents", columnDefinition = "TEXT")
 	private String contents;
 
 	@Column(name = "ts", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
 	private Timestamp ts;
-
-	public TimelineComment(){
-		
-	}
-	
-	public TimelineComment(Timeline timeline, User user, String contents){
-		this.timeline = timeline;
-		this.user = user;
-		this.contents = contents;
-	}
-
 }

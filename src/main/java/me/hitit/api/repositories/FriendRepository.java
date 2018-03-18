a6be7f740088fbc9051b4b8725eb9ec841f2c034
cheol jin Kang <kcj091857@gmail.com>
2018-03-18 21:57:04 +0900

    Modify repository related a friend class.
    Create getFriends updateFriend, getFindFriend query.

--- src/main/java/me/hitit/api/repositories/FriendRepository.java
+++ src/main/java/me/hitit/api/repositories/FriendRepository.java
@@ -2,9 +2,10 @@ package me.hitit.api.repositories;
 
 import me.hitit.api.domains.Friend;
 import me.hitit.api.domains.primary_keys.FriendPrimaryKey;
+import me.hitit.api.repositories.querydsls.interfaces.FriendQuerydslInterface;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FriendRepository extends JpaRepository<Friend, FriendPrimaryKey> {
+public interface FriendRepository extends JpaRepository<Friend, FriendPrimaryKey>, FriendQuerydslInterface {
 }

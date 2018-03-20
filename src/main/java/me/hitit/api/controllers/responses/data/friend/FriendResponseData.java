951cc555a75bd680287ca6f1831cdad43cad1e13
cheol jin Kang <kcj091857@gmail.com>
2018-03-20 13:47:25 +0900

    Modify friendResponseData class.
    FriendResponseData.java in create variable.
    GetFriendResponseData.java in create objects.

--- src/main/java/me/hitit/api/controllers/responses/data/friend/FriendResponseData.java
+++ src/main/java/me/hitit/api/controllers/responses/data/friend/FriendResponseData.java
@@ -1,4 +1,16 @@
 package me.hitit.api.controllers.responses.data.friend;
 
+import lombok.NonNull;
+
 public class FriendResponseData {
+    @NonNull
+    private Long fuidx;
+    @NonNull
+    private String name;
+    @NonNull
+    private String phoneNumber;
+    @NonNull
+    private String email;
+    @NonNull
+    private Boolean checkBen;
 }
--- src/main/java/me/hitit/api/controllers/responses/data/friend/GetFriendResponseData.java
+++ src/main/java/me/hitit/api/controllers/responses/data/friend/GetFriendResponseData.java
@@ -1,4 +1,8 @@
 package me.hitit.api.controllers.responses.data.friend;
 
+import java.util.List;
+
 public class GetFriendResponseData {
+    private List<FriendResponseData> friendResponseDatas;
+    private FriendResponseData friendResponseData;
 }

2c30040b2fc1129673760532643661d0d04d4159
cheol jin Kang <kcj091857@gmail.com>
2018-03-20 17:00:10 +0900

    Modify friendController class.
    friendController.java in add function getFriends, findFriends, updateFriendBlock.
    friendController.java 76 line replace friend/{uidx}/block{status} to friend/block/{fuidx}.

--- src/main/java/me/hitit/api/controllers/FriendController.java
+++ src/main/java/me/hitit/api/controllers/FriendController.java
@@ -2,24 +2,48 @@ package me.hitit.api.controllers;
 
 import io.swagger.annotations.Api;
 import me.hitit.api.controllers.forms.AddFriendsForm;
+import me.hitit.api.controllers.forms.UpdateFriendBlockForm;
 import me.hitit.api.controllers.responses.DefaultResponse;
+import me.hitit.api.controllers.responses.data.friend.FriendResponseData;
+import me.hitit.api.controllers.responses.data.friend.GetFriendResponseData;
+import me.hitit.api.domains.Friend;
+import me.hitit.api.domains.User;
+import me.hitit.api.services.FriendService;
 import me.hitit.api.utils.auth.Auth;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("")
 @Api
 public class FriendController {
+    @Autowired
+    FriendService fs;
+
     @GetMapping("friends")
     @Auth
     @ResponseBody
     public ResponseEntity<DefaultResponse> getFriends(
             @RequestHeader("Authorization") String jwt, @RequestParam("page") Long page,
-            @RequestParam("sort") String sort) {
-        DefaultResponse dr = new DefaultResponse();
-        return new ResponseEntity<DefaultResponse>(dr, HttpStatus.OK);
+            @RequestParam("sort") String sort, @ApiIgnore User u) throws Exception {
+        List<Friend> friends = fs.getFriends(u.getIdx(), sort, page);
+        List<FriendResponseData> frds = friends.stream().map(f -> FriendResponseData.builder()
+                .fuidx(f.getFriendPk().getFriendUser().getIdx())
+                .email(f.getFriendPk().getFriendUser().getEmail())
+                .name(f.getFriendPk().getFriendUser().getName())
+                .phoneNumber(f.getFriendPk().getFriendUser().getPhoneNumber())
+                .checkBlock(f.getCheckBlock())
+                .build())
+                .collect(Collectors.toList());
+        GetFriendResponseData gfrd = GetFriendResponseData.builder().friendResponseDatas(frds).build();
+        return new ResponseEntity<>(new DefaultResponse(gfrd), HttpStatus.OK);
     }
 
     @GetMapping("friends/find/{keyword}")
@@ -27,9 +51,18 @@ public class FriendController {
     @ResponseBody
     public ResponseEntity<DefaultResponse> findFriends(
             @RequestHeader("Authorization") String jwt, @RequestParam("page") Long page,
-            @RequestParam("sort") String sort, @PathVariable("keyword") String keyword) {
-        DefaultResponse dr = new DefaultResponse();
-        return new ResponseEntity<DefaultResponse>(dr, HttpStatus.OK);
+            @RequestParam("sort") String sort, @PathVariable("keyword") String keyword, @ApiIgnore User u) {
+        List<Friend> friends = fs.getFindFriends(u.getIdx(), sort, page, keyword);
+        List<FriendResponseData> frds = friends.stream().map(f -> FriendResponseData.builder()
+                .fuidx(f.getFriendPk().getFriendUser().getIdx())
+                .email(f.getFriendPk().getFriendUser().getEmail())
+                .name(f.getFriendPk().getFriendUser().getName())
+                .phoneNumber(f.getFriendPk().getFriendUser().getPhoneNumber())
+                .checkBlock(f.getCheckBlock())
+                .build())
+                .collect(Collectors.toList());
+        GetFriendResponseData gfrd = GetFriendResponseData.builder().friendResponseDatas(frds).build();
+        return new ResponseEntity<>(new DefaultResponse(gfrd), HttpStatus.OK);
     }
 
     @PutMapping("friends")
@@ -40,13 +73,23 @@ public class FriendController {
         return new ResponseEntity<DefaultResponse>(dr, HttpStatus.OK);
     }
 
-    @PutMapping("friend/{uidx}/block/{status}")
+    @PutMapping("friend/block/{fuidx}")
     @Auth
     @ResponseBody
     public ResponseEntity<DefaultResponse> updateFriendBlock(
-            @RequestHeader("Authorization") String jwt, @PathVariable("uidx") String uidx,
-            @PathVariable("status") Boolean status) {
-        DefaultResponse dr = new DefaultResponse();
-        return new ResponseEntity<DefaultResponse>(dr, HttpStatus.OK);
+            @Valid @RequestHeader("Authorization") String jwt, @PathVariable("fuidx") Long fuidx,
+            @ApiIgnore User u, @RequestBody UpdateFriendBlockForm ufbf) throws Exception {
+        Friend f = fs.updateFriendBlock(u.getIdx(), fuidx, ufbf);
+        FriendResponseData frd = FriendResponseData.builder()
+                .fuidx(f.getFriendPk().getFriendUser().getIdx())
+                .email(f.getFriendPk().getFriendUser().getEmail())
+                .name(f.getFriendPk().getFriendUser().getName())
+                .phoneNumber(f.getFriendPk().getFriendUser().getPhoneNumber())
+                .checkBlock(f.getCheckBlock())
+                .build();
+        GetFriendResponseData gfrd = GetFriendResponseData.builder()
+                .friendResponseData(frd)
+                .build();
+        return new ResponseEntity<>(new DefaultResponse(gfrd), HttpStatus.OK);
     }
 }

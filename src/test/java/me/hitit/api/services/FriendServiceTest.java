a73c7b9a2929cd4bf1122fd1d3d19adb1da9c0a2
cheol jin Kang <kcj091857@gmail.com>
2018-03-20 17:05:53 +0900

    Modify FriendControllerTest class.
    Create test functions getFriends, getFindFriend, updateFriendBlock.

--- src/test/java/me/hitit/api/services/FriendServiceTest.java
+++ src/test/java/me/hitit/api/services/FriendServiceTest.java
@@ -1,4 +1,10 @@
 package me.hitit.api.services;
 
-public class FriendService {
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendServiceTest {
 }

f7f805e51bbbd3b22bf8617767992406ee242caf
cheol jin Kang <kcj091857@gmail.com>
2018-03-20 16:45:18 +0900

    Update Friend Form class and responseData class.
    Update UpdateFriendBlockForm.java, FriendResponseData.java, GetFriendResponseData.java files in add @NonNull annotaion.

--- src/main/java/me/hitit/api/controllers/forms/UpdateFriendBlockForm.java
+++ src/main/java/me/hitit/api/controllers/forms/UpdateFriendBlockForm.java
@@ -3,13 +3,12 @@ package me.hitit.api.controllers.forms;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotNull;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateFriendBlockForm {
-    @NotNull
-    private Boolean checkBen;
+    @NonNull
+    private Boolean checkBlock;
 }

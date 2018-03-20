4fda3d8c5af75852bf5d7ecff2943dfc3c915dbd
cheol jin Kang <kcj091857@gmail.com>
2018-03-20 13:40:54 +0900

    Modify friend service class in functions.
    create friendService.java in create functions getFriends, updateFriendBlock, getFindFriends.

--- src/main/java/me/hitit/api/services/FriendService.java
+++ src/main/java/me/hitit/api/services/FriendService.java
@@ -1,10 +1,13 @@
 package me.hitit.api.services;
 
+import me.hitit.api.controllers.forms.UpdateFriendBlockForm;
 import me.hitit.api.domains.Friend;
 import me.hitit.api.repositories.FriendRepository;
 import me.hitit.api.services.interfaces.FriendServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("FriendService")
 public class FriendService implements FriendServiceInterface {
@@ -14,5 +17,31 @@ public class FriendService implements FriendServiceInterface {
     @Override
     public void addFriend(final Friend f) {
         fr.save(f);
+    }
+
+    @Override
+    public List<Friend> getFriends(Long tuidx, String sort, Long page) {
+        String[] resultSorts = sort.split(",");
+        return fr.getFriends(tuidx, resultSorts, page);
+    }
+
+    @Override
+    public List<Friend> getFindFriends(Long tuidx, String sort, Long page, String keyword) {
+        String[] resultSorts = sort.split(",");
+        return fr.getFindFriends(tuidx, resultSorts, page, keyword);
+    }
+
+    @Override
+    public Friend updateFriendBlock(Long tuidx, Long fuidx, UpdateFriendBlockForm ufbf) {
+        Friend f = fr.updateFriend(tuidx, fuidx);
+        if (f.getCheckBlock() == true) {
+            ufbf.setCheckBen(false);
+            f.setCheckBlock(ufbf.getCheckBen());
+        } else if (f.getCheckBlock() == false) {
+            ufbf.setCheckBen(true);
+            f.setCheckBlock(ufbf.getCheckBen());
+        }
+        fr.save(f);
+        return f;
     }
 }
--- src/main/java/me/hitit/api/services/interfaces/FriendServiceInterface.java
+++ src/main/java/me/hitit/api/services/interfaces/FriendServiceInterface.java
@@ -1,7 +1,16 @@
 package me.hitit.api.services.interfaces;
 
+import me.hitit.api.controllers.forms.UpdateFriendBlockForm;
 import me.hitit.api.domains.Friend;
+
+import java.util.List;
 
 public interface FriendServiceInterface {
     void addFriend(final Friend f);
+
+    List<Friend> getFriends(Long tuidx, String sort, Long page);
+
+    Friend updateFriendBlock(Long tuidx, Long fuidx, UpdateFriendBlockForm ufbf);
+
+    List<Friend> getFindFriends(Long tuidx, String sort, Long page, String keyword);
 }

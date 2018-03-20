aa863f76265132c8224dcbf117dd7fdeb874b78f
cheol jin Kang <kcj091857@gmail.com>
2018-03-20 13:40:54 +0900

    Modify friend service class in functions.
    create friendService.java in create functions getFriends, updateFriendBlock, getFindFriends.

--- src/main/java/me/hitit/api/controllers/forms/UpdateFriendBlockForm.java
+++ src/main/java/me/hitit/api/controllers/forms/UpdateFriendBlockForm.java
@@ -1,4 +1,15 @@
 package me.hitit.api.controllers.forms;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateFriendBlockForm {
+    @NotNull
+    private Boolean checkBen;
 }

e86f55b920fa3d0857e27fd3212bea2476420b87
sjaqjwor <lsklsk45@naver.com>
2018-03-22 18:23:05 +0900

    Solve merge conflict 'UpdateTimelineCommentForm' class
    solve merge conflict

--- src/main/java/me/hitit/api/controllers/forms/UpdateTimelineCommentForm.java
+++ src/main/java/me/hitit/api/controllers/forms/UpdateTimelineCommentForm.java
@@ -5,28 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-<<<<<<< HEAD
-import javax.validation.constraints.NotNull;
-
-/**
- * UpdateTimelineCommentForm class.
- * 
- * @author devetude
- */
-=======
->>>>>>> fe7a64dc19d961cc8f8b4e0e5068ac9b187684bf
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateTimelineCommentForm {
-<<<<<<< HEAD
-	private static final Logger LOG = Logger.getLogger(UpdateTimelineCommentForm.class.getSimpleName());
-
-	@NotNull
-	private String contents;
-}
-=======
     @NonNull
     private String contents;
 }
->>>>>>> fe7a64dc19d961cc8f8b4e0e5068ac9b187684bf
+

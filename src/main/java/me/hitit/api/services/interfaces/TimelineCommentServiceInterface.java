ce1fcd29b42b06e84231cbe32d6af28b52b93f8c
sjaqjwor <lsklsk45@naver.com>
2018-03-22 18:43:37 +0900

    Modify 'TImelineCommentServiceInterface' class
    remove remark

--- src/main/java/me/hitit/api/services/interfaces/TimelineCommentServiceInterface.java
+++ src/main/java/me/hitit/api/services/interfaces/TimelineCommentServiceInterface.java
@@ -8,33 +8,11 @@ import me.hitit.api.domains.User;
 import java.util.List;
 
 public interface TimelineCommentServiceInterface {
-    /**
-     * Methods to get a timelineComment by index.
-     */
-    public List<TimelineComment> getTimelineComment(final Long tidx, final String sort, final Long page);
+    List<TimelineComment> getTimelineComment(final Long tidx, final String sort, final Long page);
 
-    /**
-     * Methods to add a timelineComment by index.
-     *
-     * @param u
-     * @param atcf
-     */
-    public void addTimelineComment(final User u, AddTimelineCommentsForm atcf);
+    void addTimelineComment(final User u, AddTimelineCommentsForm atcf);
 
-    /**
-     * Methods to update a timelineComment by index.
-     *
-     * @param u
-     * @param utcf
-     * @param tidx
-     * @param tcidx
-     */
-    public void updateTimelineComment(final User u, final UpdateTimelineCommentForm utcf, Long tidx, Long tcidx);
-    /**
-     * Methods to update a timelineComment by index.
-     *
-     * @param tidx
-     * @param tcidx
-     */
-    public void deleteTimelineComment(final Long tcidx, final Long tidx);
+    void updateTimelineComment(final User u, final UpdateTimelineCommentForm utcf, Long tidx, Long tcidx);
+
+    void deleteTimelineComment(final Long tcidx, final Long tidx);
 }

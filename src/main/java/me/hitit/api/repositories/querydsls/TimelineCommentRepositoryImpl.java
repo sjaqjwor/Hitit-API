892a0f50468774d6fea87f7b95a8754ef8e8fb1f
sjaqjwor <lsklsk45@naver.com>
2018-03-22 18:41:45 +0900

    Modify 'TimelineCommentReopositoryImpl' class
    remove LOG

--- src/main/java/me/hitit/api/repositories/querydsls/TimelineCommentRepositoryImpl.java
+++ src/main/java/me/hitit/api/repositories/querydsls/TimelineCommentRepositoryImpl.java
@@ -12,37 +12,35 @@ import me.hitit.api.domains.TimelineComment;
 import me.hitit.api.repositories.querydsls.interfaces.TimelineCommentQuerydslInterface;
 import org.apache.log4j.Logger;
 import org.springframework.data.jpa.repository.support.QueryDslRepositorySupport;
-import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 public class TimelineCommentRepositoryImpl extends QueryDslRepositorySupport implements TimelineCommentQuerydslInterface {
-private static final Logger LOG = Logger.getLogger(TimelineCommentRepositoryImpl.class);
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	private JPAQueryFactory jqf = new JPAQueryFactory(this.getEntityManager());
-	private QTimelineComment qtc = QTimelineComment.timelineComment;
-	private QUser qu = QUser.user;
+    private JPAQueryFactory jqf = new JPAQueryFactory(this.getEntityManager());
+    private QTimelineComment qtc = QTimelineComment.timelineComment;
+    private QUser qu = QUser.user;
 
-	public TimelineCommentRepositoryImpl() {
-		super(TimelineComment.class);
-	}
+    public TimelineCommentRepositoryImpl() {
+        super(TimelineComment.class);
+    }
 
-	@Override
-	public List<TimelineComment> getTimelineComment(Long tidx, String[] sorts, Long page) {
-		OrderSpecifier obj[] = new OrderSpecifier[sorts.length];
-		for (int a = 0; a < sorts.length; a++) {
-			Order order = sorts[a].substring(0, 1).equals("+") ? Order.ASC : Order.DESC;
-			SimplePath<Object> filedPath = Expressions.path(Object.class, qtc,
-					sorts[a].substring(1, sorts[a].length()));
-			obj[a] = new OrderSpecifier(order, filedPath);
-		}
+    @Override
+    public List<TimelineComment> getTimelineComment(Long tidx, String[] sorts, Long page) {
+        OrderSpecifier obj[] = new OrderSpecifier[sorts.length];
+        for (int a = 0; a < sorts.length; a++) {
+            Order order = sorts[a].substring(0, 1).equals("+") ? Order.ASC : Order.DESC;
+            SimplePath<Object> filedPath = Expressions.path(Object.class, qtc,
+                    sorts[a].substring(1, sorts[a].length()));
+            obj[a] = new OrderSpecifier(order, filedPath);
+        }
 
-		return from(qtc).join(qtc.user, qu).where(qtc.timeline.idx.eq(tidx)).orderBy(obj).limit(page)
-				.fetch();
-	}
+        return from(qtc).join(qtc.user, qu).where(qtc.timeline.idx.eq(tidx)).orderBy(obj).limit(page)
+                .fetch();
+    }
 }

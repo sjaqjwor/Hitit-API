12f9d7a982a8a505b28f421e09e762a0a6c95f76
CheolJin Kang <kcj091857@gmail.com>
2018-03-28 22:04:11 +0900

    Create file FriendDTO class
    Create file FriendDTO.java

---
+++ src/main/java/me/hitit/api/dtos/FriendDTO.java
@@ -0,0 +1,18 @@
+package me.hitit.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FriendDTO {
+    private Long fuidx;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private Boolean checkBlock;
+}

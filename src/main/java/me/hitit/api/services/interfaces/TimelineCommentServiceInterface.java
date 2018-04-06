05f65b81af7840a9c3f21df39c859b0dd4660f35
sjaqjwor <lsklsk45@naver.com>
2018-04-06 11:51:33 +0900

    Modify 'TimelineCommentServiceInterface' class
    delete final keyword and add @NonNull annotation

--- src/main/java/me/hitit/api/services/interfaces/TimelineCommentServiceInterface.java
+++ src/main/java/me/hitit/api/services/interfaces/TimelineCommentServiceInterface.java
@@ -1,18 +1,20 @@
 package me.hitit.api.services.interfaces;
 
+import lombok.NonNull;
 import me.hitit.api.controllers.forms.AddTimelineCommentsForm;
 import me.hitit.api.controllers.forms.UpdateTimelineCommentForm;
 import me.hitit.api.controllers.responses.data.timelinecomment.TimelineCommentResponseData;
 import me.hitit.api.domains.User;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public interface TimelineCommentServiceInterface {
-    List<TimelineCommentResponseData> getTimelineComment(final Long tidx, final String sort, final Long page);
+    List<TimelineCommentResponseData> getTimelineComment(@NonNull Long tidx,@NonNull String sort,@NonNull Long page);
 
-    void addTimelineComment(final User u, AddTimelineCommentsForm atcf);
+    void addTimelineComment(@NonNull User u,@NonNull AddTimelineCommentsForm atcf);
 
-    void updateTimelineComment(final User u, final UpdateTimelineCommentForm utcf, Long tidx, Long tcidx);
+    void updateTimelineComment(@NonNull User u,@NonNull UpdateTimelineCommentForm utcf,@NonNull Long tidx,@NonNull Long tcidx);
 
-    void deleteTimelineComment(final Long tcidx, final Long tidx);
+    void deleteTimelineComment(@NonNull Long tcidx,@NonNull Long tidx);
 }

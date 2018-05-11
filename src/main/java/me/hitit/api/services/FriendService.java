747a2e721943c3a502f7a120327ec86a66d98806
CheolJin Kang <kcj091857@gmail.com>
2018-05-11 15:05:58 +0900

    Modify FiendService functions
    Create Function getBlockFriends
    Apply 'dto' to all functions

--- src/main/java/me/hitit/api/services/FriendService.java
+++ src/main/java/me/hitit/api/services/FriendService.java
@@ -1,10 +1,10 @@
 package me.hitit.api.services;
 
 import lombok.NonNull;
-import me.hitit.api.controllers.forms.UpdateFriendBlockForm;
+import me.hitit.api.controllers.responses.data.friend.BlockFriendsResponseData;
 import me.hitit.api.controllers.responses.data.friend.FriendResponseData;
 import me.hitit.api.domains.Friend;
-import me.hitit.api.dtos.FriendDTO;
+import me.hitit.api.dtos.FriendDto;
 import me.hitit.api.repositories.FriendRepository;
 import me.hitit.api.services.interfaces.FriendServiceInterface;
 import org.modelmapper.ModelMapper;
@@ -27,52 +27,96 @@ public class FriendService implements FriendServiceInterface {
     }
 
     @Override
-    public List<Friend> getFriends(Long tuidx, String sort, Long page) {
-        return fr.getFriends(tuidx, sort.split(","), page);
-    }
-
-    @Override
-    public List<Friend> getFindFriends(Long tuidx, String sort, Long page, String keyword) {
-        return fr.getFindFriends(tuidx, sort.split(","), page, keyword);
-    }
-
-    @Override
-    public Friend updateFriendBlock(Long tuidx, Long fuidx, UpdateFriendBlockForm ufbf) {
-        Friend f = fr.updateFriend(tuidx, fuidx);
-        if (f.getIsBlock()) {
-            ufbf.setIsBlock(false);
-            f.setIsBlock(ufbf.getIsBlock());
-        } else if (!f.getIsBlock()) {
-            ufbf.setIsBlock(true);
-            f.setIsBlock(ufbf.getIsBlock());
-        }
-        fr.save(f);
-        return f;
-    }
-
-    @Override
-    public List<FriendResponseData> getFriendsDto(Long tuidx, String sort, Long page) {
-        List<Friend> friends = fr.getFriends(tuidx, sort.split(","), page);
+    public List<FriendResponseData> getFindFriends(Long tuidx, String sort, Long page, String keyword) {
+        List<Friend> friends = fr.getFindFriends(tuidx, sort.split(","), page, keyword);
+        int index = 0;
         ModelMapper mm = new ModelMapper();
-        List<FriendDTO> friendDtos = mm.map(friends, new TypeToken<List<FriendDTO>>() {
+        List<FriendDto> friendDto = mm.map(friends, new TypeToken<List<FriendDto>>() {
         }.getType());
-        for (int i = 0; i < friends.size(); i++) {
-            Friend friend = friends.get(i);
-            FriendDTO dto = friendDtos.get(i);
-            dto.setFuidx(friend.getFriendPk().getFriendUser().getIdx());
-            dto.setName(friend.getFriendPk().getFriendUser().getName());
-            dto.setEmail(friend.getFriendPk().getFriendUser().getEmail());
-            dto.setPhoneNumber(friend.getFriendPk().getFriendUser().getPhoneNumber());
-            dto.setIsBlock(friend.getIsBlock());
+        for (Friend f : friends) {
+            Friend friend = friends.get(index);
+            FriendDto fdto = friendDto.get(index);
+            fdto.setFuidx(friend.getFriendPk().getFriendUser().getIdx());
+            fdto.setName(friend.getFriendPk().getFriendUser().getName());
+            fdto.setEmail(friend.getFriendPk().getFriendUser().getEmail());
+            fdto.setPhoneNumber(friend.getFriendPk().getFriendUser().getPhoneNumber());
+            fdto.setIsBlock(friend.getIsBlock());
+            index++;
         }
-        List<FriendResponseData> frds = friendDtos.stream().map(f -> FriendResponseData.builder()
+        List<FriendResponseData> frds = friendDto.stream().map(f -> FriendResponseData.builder()
                 .fuidx(f.getFuidx())
                 .email(f.getEmail())
                 .name(f.getName())
                 .phoneNumber(f.getPhoneNumber())
-                .checkBlock(f.getIsBlock())
                 .build())
                 .collect(Collectors.toList());
         return frds;
+    }
+
+    @Override
+    public void updateFriendBlock(Long tuidx, Long fuidx, Boolean changeBlock) {
+        Friend f = fr.updateFriend(tuidx, fuidx);
+        if (f.getIsBlock()) {
+            changeBlock = false;
+            f.setIsBlock(changeBlock);
+        } else if (!f.getIsBlock()) {
+            changeBlock = true;
+            f.setIsBlock(changeBlock);
+        }
+        fr.save(f);
+    }
+
+    @Override
+    public List<FriendResponseData> getFriends(Long tuidx, String sort, Long page) {
+        List<Friend> friends = fr.getFriends(tuidx, sort.split(","), page);
+        int index = 0;
+        ModelMapper mm = new ModelMapper();
+        List<FriendDto> friendDto = mm.map(friends, new TypeToken<List<FriendDto>>() {
+        }.getType());
+        for (Friend f : friends) {
+            Friend friend = friends.get(index);
+            FriendDto fdto = friendDto.get(index);
+            fdto.setFuidx(friend.getFriendPk().getFriendUser().getIdx());
+            fdto.setName(friend.getFriendPk().getFriendUser().getName());
+            fdto.setEmail(friend.getFriendPk().getFriendUser().getEmail());
+            fdto.setPhoneNumber(friend.getFriendPk().getFriendUser().getPhoneNumber());
+            fdto.setIsBlock(friend.getIsBlock());
+            index++;
+        }
+        List<FriendResponseData> frds = friendDto.stream().map(f -> FriendResponseData.builder()
+                .fuidx(f.getFuidx())
+                .email(f.getEmail())
+                .name(f.getName())
+                .phoneNumber(f.getPhoneNumber())
+                .build())
+                .collect(Collectors.toList());
+        return frds;
+    }
+
+    @Override
+    public List<BlockFriendsResponseData> getBlockFriends(Long tuidx, String sort, Long page) {
+        List<Friend> friends = fr.getFriends(tuidx, sort.split(","), page);
+        int index = 0;
+        ModelMapper mm = new ModelMapper();
+        List<FriendDto> friendDto = mm.map(friends, new TypeToken<List<FriendDto>>() {
+        }.getType());
+        for (Friend f : friends) {
+            Friend friend = friends.get(index);
+            FriendDto fdto = friendDto.get(index);
+            fdto.setFuidx(friend.getFriendPk().getFriendUser().getIdx());
+            fdto.setName(friend.getFriendPk().getFriendUser().getName());
+            fdto.setEmail(friend.getFriendPk().getFriendUser().getEmail());
+            fdto.setPhoneNumber(friend.getFriendPk().getFriendUser().getPhoneNumber());
+            fdto.setIsBlock(friend.getIsBlock());
+            index++;
+        }
+        List<BlockFriendsResponseData> bfrds = friendDto.stream().map(f -> BlockFriendsResponseData.builder()
+                .fuidx(f.getFuidx())
+                .email(f.getEmail())
+                .name(f.getName())
+                .phoneNumber(f.getPhoneNumber())
+                .build())
+                .collect(Collectors.toList());
+        return bfrds;
     }
 }
--- src/main/java/me/hitit/api/services/interfaces/FriendServiceInterface.java
+++ src/main/java/me/hitit/api/services/interfaces/FriendServiceInterface.java
@@ -1,10 +1,9 @@
 package me.hitit.api.services.interfaces;
 
 import lombok.NonNull;
-import me.hitit.api.controllers.forms.UpdateFriendBlockForm;
+import me.hitit.api.controllers.responses.data.friend.BlockFriendsResponseData;
 import me.hitit.api.controllers.responses.data.friend.FriendResponseData;
 import me.hitit.api.domains.Friend;
-import me.hitit.api.dtos.FriendDTO;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -12,11 +11,11 @@ import java.util.List;
 public interface FriendServiceInterface {
     void addFriend(@NonNull Friend f);
 
-    List<Friend> getFriends(@NonNull Long tuidx, @NonNull String sort, @NonNull Long page);
+    void updateFriendBlock(@NonNull Long tuidx, @NonNull Long fuidx, @Nullable Boolean changeBlock);
 
-    Friend updateFriendBlock(@NonNull Long tuidx, @NonNull Long fuidx, @Nullable UpdateFriendBlockForm ufbf);
+    List<FriendResponseData> getFindFriends(@NonNull Long tuidx, @NonNull String sort, @NonNull Long page, @NonNull String keyword);
 
-    List<Friend> getFindFriends(@NonNull Long tuidx, @NonNull String sort, @NonNull Long page, @NonNull String keyword);
+    List<FriendResponseData> getFriends(@NonNull Long tuidx, @NonNull String sort, @NonNull Long page);
 
-    List<FriendResponseData> getFriendsDto(@NonNull Long tuidx, @NonNull String sort, @NonNull Long page);
+    List<BlockFriendsResponseData> getBlockFriends(@NonNull Long tuidx, @NonNull String sort, @NonNull Long page);
 }
